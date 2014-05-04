783f698fb4d6d92a1b1ad048ffbf2cb44c1b10df
Richard Querin <rfquerin@gmail.com>
2014-05-04 00:35:39 -0400

    code clean up and simplification.

--- app/src/main/java/com/rfquerin/simplebeamtool/app/MainActivity.java
+++ app/src/main/java/com/rfquerin/simplebeamtool/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.rfquerin.simplebeamtool.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -31,12 +33,29 @@ public class MainActivity extends Activity {
     public boolean isMetric = true;
     public float deadfactor = (float)1.25;
     public float livefactor = (float)1.50;
+    public float YoungsMetric = (float)200000;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // read preferences file and extract load factors and youngs modulus value
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String dfactorstring = sharedPref.getString("dead_factor","1.25");
+        String lfactorstring = sharedPref.getString("live_factor","1.50");
+        String Ystring = sharedPref.getString("youngs_modulus","200000");
+
+        deadfactor = Float.valueOf(dfactorstring);
+        livefactor = Float.valueOf(lfactorstring);
+        YoungsMetric = Float.valueOf(Ystring);
+
+
+
 
         // arbitrarily hit refresh to update values on creation
 
@@ -68,6 +87,37 @@ public class MainActivity extends Activity {
 		
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // refresh units to handle device
+        // orientation change
+
+        // put  some code here to do it
+
+        // determine whether metric or imperial
+
+        RadioButton unitsSI = (RadioButton)findViewById(R.id.radioButtonSI);
+
+        if (unitsSI.isChecked()) {
+
+            setUnitsMetric();
+
+        }
+        else
+        {
+
+            setUnitsImperial();
+        }
+
+        // arbitrarily hit refresh to update values on creation
+
+        Button refreshbutton = (Button)findViewById(R.id.buttonRefresh);
+        refreshbutton.performClick();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -95,7 +145,7 @@ public class MainActivity extends Activity {
 
         // handle radio button click
 
-        // --test --TextView deflunits = (TextView)findViewById(R.id.textViewDeflnUnits);
+
         TextView spanunits = (TextView)findViewById(R.id.textViewSpanUnits);
         TextView tribunits = (TextView)findViewById(R.id.textViewTribUnits);
         TextView deadunits = (TextView)findViewById(R.id.textViewDeadUnits);
@@ -110,6 +160,7 @@ public class MainActivity extends Activity {
         EditText deadtext = (EditText)findViewById(R.id.editTextDead);
         EditText livetext = (EditText)findViewById(R.id.editTextLive);
         TextView deflntext = (TextView)findViewById(R.id.textViewDefln);
+        EditText deflnratiotext = (EditText)findViewById(R.id.editTextDeflnRatio);
 
         TextView momenttext = (TextView)findViewById(R.id.textViewMoment);
         TextView reactiontext = (TextView)findViewById(R.id.textViewReaction);
@@ -119,12 +170,6 @@ public class MainActivity extends Activity {
 
 
 
-
-
-
-        // Is the button now checked?
-        boolean checked = ((RadioButton) v).isChecked();
-
         // set formatting for values
 
         DecimalFormat df3 = new DecimalFormat("0.000");
@@ -132,22 +177,16 @@ public class MainActivity extends Activity {
         DecimalFormat df1 = new DecimalFormat("0.0");
         DecimalFormat df_IxSI = new DecimalFormat("##0.###E0");
 
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) v).isChecked();
+
+
+
         // Check which radio button was clicked
         switch(v.getId()) {
             case R.id.radioButtonSI:
                 if (checked)
-                    // deflunits.setText("METRIC"); -- test case
-                    // set all textviews with units to be metric ones
-
-                    spanunits.setText(R.string.span_units_si);
-                    tribunits.setText(R.string.tribwidth_units_si);
-                    deadunits.setText(R.string.deadUDL_units_si);
-                    liveunits.setText(R.string.liveUDL_units_si);
-                    deflunits.setText(R.string.defln_units_si);
-                    momentunits.setText(R.string.Mf_units_si);
-                    reactionunits.setText(R.string.Rf_units_si);
-                    inertiaunits1.setText(R.string.Ix_units1_si);
-
 
                     if (!isMetric) // if current state was imperial
                     {
@@ -165,17 +204,17 @@ public class MainActivity extends Activity {
                         float liveimp = Float.valueOf(livetext.getText().toString());
                         float livemetric = liveimp / (float)20.88;
 
-                        float deflnimp = Float.valueOf(deflntext.getText().toString());
-                        float deflnmetric = deflnimp * (float)25.4;
+                        float deflnratio = Float.valueOf(deflnratiotext.getText().toString());
 
-                        float momentimp = Float.valueOf(momenttext.getText().toString());
-                        float momentmetric = momentimp * (float)1.355818;
+                        float deflnmetric = spanmetric * (float)1000.0 / deflnratio;
 
-                        float reactionimp = Float.valueOf(reactiontext.getText().toString());
-                        float reactionmetric = reactionimp * (float)1000 / (float)225;
 
-                        float inertiaimp = Float.valueOf(inertiatext.getText().toString());
-                        float inertiametric = inertiaimp * (float)25.4 * (float)25.4 * (float)25.4 * (float)25.4;
+
+                        float momentmetric = calcMomentMetric(spanmetric, tribmetric, deadmetric, livemetric);
+                        float reactionmetric = calcReactionMetric(spanmetric, tribmetric, deadmetric, livemetric);
+                        float inertiametric = calcInertiaMetric(spanmetric, tribmetric, livemetric, deflnratio);
+
+
 
 
 
@@ -211,6 +250,13 @@ public class MainActivity extends Activity {
                         momenttext.setText(formattedmoment);
                         reactiontext.setText(formattedreaction);
                         inertiatext.setText(formattedinertia);
+
+
+
+                        // set all textviews with units to be metric ones
+
+                        setUnitsMetric();
+
 
 
 
@@ -258,18 +304,15 @@ public class MainActivity extends Activity {
                     float livemetric = Float.valueOf(livetext.getText().toString());
                     float liveimp = livemetric * (float)20.88;
 
-                    float deflnmetric = Float.valueOf(deflntext.getText().toString());
-                    float deflnimp = deflnmetric / (float)25.4;
+                    float deflnratio = Float.valueOf(deflnratiotext.getText().toString());
 
-                    float momentmetric = Float.valueOf(momenttext.getText().toString());
-                    float momentimp = momentmetric / (float)1.355818;
+                    float deflnimp = spanimp * (float)12.0 / deflnratio;
 
-                    float reactionmetric = Float.valueOf(reactiontext.getText().toString());
-                    float reactionimp = reactionmetric * (float)0.225;
 
-                    float inertiametric = Float.valueOf(inertiatext.getText().toString());
-                    float inertiaimp = inertiametric / ((float)25.4 * (float)25.4 * (float)25.4 * (float)25.4);
 
+                    float momentimp = calcMomentImperial(spanimp, tribimp, deadimp, liveimp);
+                    float reactionimp = calcReactionImperial(spanimp, tribimp, deadimp, liveimp);
+                    float inertiaimp = calcInertiaImperial(spanimp, tribimp, liveimp, deflnratio);
 
 
                     // format values into strings
@@ -341,37 +384,26 @@ public class MainActivity extends Activity {
 
         // handle moment, reaction and inertia calculations
 
-        // determine whether metric or imperial
 
-        RadioButton unitsSI = (RadioButton)findViewById(R.id.radioButtonSI);
-
-        if (unitsSI.isChecked()) {
+        if (isMetric) {
 
 
             // compute bending moment
 
-            float load = ((deadfactor * deadload) + (livefactor * liveload)) * tribwidth;
+            float moment = calcMomentMetric(span,tribwidth,deadload,liveload);
 
-            float moment = (load * span * span) * (float) 0.125;
-
-            float liveload_linear = liveload * tribwidth;
 
             // compute end reaction
 
-            float reaction = (load * span) * (float) 0.50;
+            float reaction = calcReactionMetric(span,tribwidth,deadload,liveload);
 
 
             // compute required moment of inertia
 
-            float span_mm = (float) 1000.0 * span;
+            float inertia = calcInertiaMetric(span,tribwidth,liveload,deflnRatio);
 
-            float defln_measurement = span_mm / deflnRatio;
+            float defln_measurement = span * (float)1000.0 / deflnRatio;
 
-            float numerator = (float) 5.0 * liveload_linear * span_mm * span_mm * span_mm * span_mm;
-
-            float denominator = (float) 384 * (float) 200000 * defln_measurement;
-
-            float inertia = numerator / denominator;
 
 
             // format moment and reaction values into strings with only 1 decimal place
@@ -387,21 +419,21 @@ public class MainActivity extends Activity {
             // put moment, reaction, and inertia values into appropriate textview widgets
 
             TextView momentField = (TextView) findViewById(R.id.textViewMoment);
-            String momentString = formattedmoment;
+
 
             TextView reactionField = (TextView) findViewById(R.id.textViewReaction);
-            String reactionString = formattedreaction;
+
 
             TextView inertiaField = (TextView) findViewById(R.id.textViewIx);
-            String inertiaString = formattedinertia;
+
 
             TextView deflectionField = (TextView) findViewById(R.id.textViewDefln);
-            String deflectionString = formatteddeflection;
 
-            momentField.setText(momentString);
-            reactionField.setText(reactionString);
-            inertiaField.setText(inertiaString);
-            deflectionField.setText(deflectionString);
+
+            momentField.setText(formattedmoment);
+            reactionField.setText(formattedreaction);
+            inertiaField.setText(formattedinertia);
+            deflectionField.setText(formatteddeflection);
         }
         else
         {
@@ -410,30 +442,20 @@ public class MainActivity extends Activity {
 
             // compute bending moment
 
-            float load = ((deadfactor * deadload / (float)1000) + (livefactor * liveload / (float)1000)) * tribwidth;
-
-            float moment = (load * span * span) * (float) 0.125;
+            float moment = calcMomentImperial(span,tribwidth,deadload,liveload);
 
 
 
             // compute end reaction
 
-            float reaction = (load * span) * (float) 0.50;
+            float reaction = calcReactionImperial(span,tribwidth,deadload,liveload);
 
 
             // compute required moment of inertia
 
-            float liveload_linear = (float)0.001 * liveload * tribwidth / (float)12;
+            float inertia = calcInertiaImperial(span, tribwidth,liveload,deflnRatio);
 
-            float span_in = (float) 12 * span;
-
-            float defln_measurement = span_in / deflnRatio;
-
-            float numerator = (float) 5.0 * liveload_linear * span_in * span_in * span_in * span_in;
-
-            float denominator = (float) 384 * (float) 29000 * defln_measurement;
-
-            float inertia = numerator / denominator;
+            float defln_measurement = span * (float) 12.0 / deflnRatio;
 
 
             // format moment and reaction values into strings with only 1 decimal place
@@ -494,6 +516,8 @@ public class MainActivity extends Activity {
         reactionunits.setText(R.string.Rf_units_si);
         inertiaunits1.setText(R.string.Ix_units1_si);
 
+        isMetric = true;
+
 
 
     }
@@ -521,6 +545,86 @@ public class MainActivity extends Activity {
         reactionunits.setText(R.string.Rf_units_imp);
         inertiaunits1.setText(R.string.Ix_units1_imp);
 
+        isMetric = false;
+
+
 
     }
+
+
+    public float calcMomentMetric(float span, float trib, float dead, float live){
+
+        // calculation of bending moment in metric (span in m, trib in m, dead in kPa, live in kPa)
+
+        float wf = ((deadfactor * dead)+(livefactor * live)) * trib * span * span / (float)8.0;
+        return wf;
+    }
+
+
+    public float calcReactionMetric(float span, float trib, float dead, float live){
+
+        // calculation of beam reaction in metric (span in m, trib in m, dead in kPa, live in kPa)
+
+        float Rf = ((deadfactor * dead)+(livefactor * live)) * trib * span / (float)2.0;
+        return Rf;
+    }
+
+    public float calcInertiaMetric(float span, float trib, float live, float deflnratio){
+
+        // calculation of moment of inertia in metric (span in m, trib in m, live in kPa, defln is a ratio of the span
+        float span_mm = span * (float)1000.0; // mm
+        
+        float lineload = (live*trib); // N/mm
+
+        float numerator = (float)5.0 * lineload * span_mm * span_mm * span_mm *span_mm;
+        
+        float denominator = (float)384 * YoungsMetric * (span_mm/deflnratio);
+        
+        float Ix = numerator/denominator;
+        
+        return Ix;
+        
+                
+    }
+
+    public float calcMomentImperial(float span, float trib, float dead, float live){
+
+        // calculation of bending moment in imperial (span in ft, trib in ft, dead in psf, live in psf)
+
+        float wf = ((deadfactor * dead)+(livefactor * live))* (float)0.001 * trib * span * span / (float)8.0;
+        return wf;
+    }
+
+    public float calcReactionImperial(float span, float trib, float dead, float live){
+
+        // calculation of beam reaction in imperial (span in ft, trib in ft, dead in psf, live in psf)
+
+        float Rf = ((deadfactor * dead)+(livefactor * live))*(float)0.001 * trib * span / (float)2.0;
+        return Rf;
+    }
+
+    public float calcInertiaImperial(float span, float trib, float live, float deflnratio){
+
+        // calculation of moment of inertia in imperial (span in ft, trib in ft, live in psf, defln is a ratio of the span
+
+        float YoungsImperial = YoungsMetric * (float) 0.145161;
+
+        float span_in = span * (float)12; // in
+
+        float live_ksf = live * (float)0.001 * live; // ksft
+
+        float lineload = (live_ksf * trib)/(float)12.0; // kips/in
+
+        float numerator = (float)5.0 * lineload * span_in * span_in * span_in * span_in;
+
+        float denominator = (float)384 * YoungsImperial * (span_in/deflnratio);
+
+        float Ix = numerator/denominator;
+
+        return Ix;
+
+
+    }
+
+
 }
